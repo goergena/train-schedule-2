4ef38f9c5b1bd264cc5532315eac50649fa87381
goergena <goergena@users.noreply.github.com>
2018-03-29 16:25:49 -0400

    bug fix wait time display

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,3 @@
-$(document).ready(function () {
-
     // Initialize Firebase
     var config = {
         apiKey: "AIzaSyAjWOkVx7_9m5A5ivUMDg7tPy6jXwPlN-Q",
@@ -26,8 +24,8 @@ $(document).ready(function () {
             firstTime: firstTime,
             frequency: frequency,
         });
-
     });
+
 
     database.ref().on("child_added", function (snapshot) {
         var data = snapshot.val();
@@ -40,13 +38,12 @@ $(document).ready(function () {
         freq.text(data.frequency);
 
         var next = $("<td>");
-        var now = moment();
         var firstArrival = moment(data.firstTime, "HH:mm");
-        var nextArrival = calculateNextArrival(firstArrival, now, data.frequency);
+        var nextArrival = calculateNextArrival(firstArrival, moment(), data.frequency);
         next.text(nextArrival.format("hh:mm A"));
 
         var wait = $("<td>");
-        wait.text(nextArrival.diff(now, "minutes"));
+        wait.text(nextArrival.diff(moment(), 'minutes'));
 
         row.append(head, dest, freq, next, wait);
         $("tbody").append(row);
@@ -56,17 +53,15 @@ $(document).ready(function () {
     });
 
 
-    function calculateNextArrival(x, y, z) {
+    function calculateNextArrival(firstTrain, now, frequency) {
         //x = firstArrival; y = now; z = freq
 
-        if (x > y) {
-            return x;
+        if (firstTrain > now) {
+            return firstTrain;
         } else {
-            var differenceTimes = y.diff(x, "minutes");
-            var tRemainder = differenceTimes % parseInt(z);
-            var waitInMin = parseInt(z) - tRemainder;
-            return y.add(waitInMin, "m");
+            var differenceTimes = now.diff(firstTrain, "minutes");
+            var tRemainder = differenceTimes % parseInt(frequency);
+            var waitInMin = parseInt(frequency) - tRemainder;
+            return now.add(waitInMin, "m");
         }
     };
-
-});
